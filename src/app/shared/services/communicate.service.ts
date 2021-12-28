82acc6cfaab8eac12d4ef23cb6408ea7e1dc8681
Stanley Wang <syw2007@gmail.com>
2021-12-27 19:18:37 -0800

    updated about page

--- src/app/shared/services/communicate.service.ts
+++ src/app/shared/services/communicate.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Client } from '../clients';
+import { google } from 'googleapis';
 
 
 /*setting up variables and links*/
@@ -15,12 +16,26 @@ export class CommunicateService {
 
   constructor(
     private http: HttpClient) { }
+
+
+  authenticate() {
+    const apikey: string = "AIzaSyDOzDFAA4qFh1-DIzsp9frBSpiAdTlix3E";
+    const clientID: string = "104639220756548749750";
+    const scopevalue: string = "https://www.googleapis.com/auth/spreadsheets";
+    const discoverydoc: string = "https://sheets.googleapis.com/$discovery/rest?version=v4";
+    const client = gapi.client.init({
+      apiKey: apikey,
+      clientId: clientID,
+      discoveryDocs: [discoverydoc],
+      scope: scopevalue
+    })
+  }
   
   uploadItem(item:Client) {
     console.log(item); 
     if (item.name == "Stanley Wang") {
       console.log("no chinamen allowed not sorry");
-        
+    this.authenticate;
     }
 
   }
